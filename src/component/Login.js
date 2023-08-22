3ec59893a808b1e0eccd325d3c8e38ebd8d240cb
priyanshusharman <priyanshuk1900@gmaul.com>
2023-08-22 21:10:06 +0530

    signup backtologin ui added

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,21 +1,35 @@
 import React from 'react'
+import Signup from './signup';
 
 export default function Login() {
+  const remove = () => {
+    const flotingbox = document.body.querySelector('.flotingbox');
+    flotingbox.style.display = 'none'
+  }
+  const signuppage = () => {
+    const flotingbox = document.body.querySelector('.loginuser');
+    const signup = document.body.querySelector('.signup');
+    flotingbox.style.display = 'none'
+    signup.style.display = 'block'
+
+  }
   return (
     <div className="flotingbox">
-      {/* <label htmlFor="cut"> */}
-        <button name="cut" id="cut" ><i class="fa-solid fa-xmark" id="cut"></i>
+      <div className="loginuser">
+        <button name="cut" id="cut" onClick={remove} ><i class="fa-solid fa-xmark" id="cut"></i>
         </button>
-      {/* <input type="checkbox" name="cut" id="cut" hidden /> */}
-      {/* <i class="fa-solid fa-xmark" id="cut"></i></label> */}
         <h1 className='login'>Login</h1>
-      <p className='username'>Username</p>
-      <input type="text" name='usernamei' id='usernamei' placeholder='username'/>
-      <p className='password'>Password</p>
-      <input type="text" name="passwordi" id="passwordi" placeholder='password' />
-      <div className="button">
-      <button id='Login'>Login</button>
-      <button id='Sign up'>Sign up</button>
+        <p className='username'>Username</p>
+        <input type="text" name='usernamei' id='usernamei' placeholder='username' />
+        <p className='password'>Password</p>
+        <input type="text" name="passwordi" id="passwordi" placeholder='password' />
+        <div className="button">
+          <button id='Login'>Login</button>
+          <button id='Sign up' onClick={signuppage}>Sign up</button>
+        </div>
+      </div>
+      <div className="signup">
+        <Signup/>
       </div>
     </div>
   )
---
+++ src/component/signup.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Signup() {
+    const remove = () => {
+        const flotingbox = document.body.querySelector('.flotingbox');
+        flotingbox.style.display = 'none'
+      }
+    const backto_log_in = () => {
+        const flotingbox = document.body.querySelector('.loginuser');
+        const signup = document.body.querySelector('.signup');
+        flotingbox.style.display = 'block'
+        signup.style.display = 'none'
+      }
+    return (
+        <div>
+            <button name="cut" id="cut" onClick={remove} ><i class="fa-solid fa-xmark" id="cut"></i>
+            </button>
+            <p className='email'>Email ID</p>
+            <input type="text" name='usernamei' id='usernamei' placeholder='Email ID' />
+            <p className='phoneno'>Phone Number</p>
+            <input type="text" name='usernamei' id='usernamei' placeholder='Email ID' />
+            <p className='password'>Password</p>
+            <input type="text" name="passwordi" id="passwordi" placeholder='password' />
+            <div className="button">
+                <button id='Signup'>Sign up</button>
+                <button id='Login' onClick={backto_log_in}>Back to Login</button>
+            </div>
+        </div>
+    )
+}
